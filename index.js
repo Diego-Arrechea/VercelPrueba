3c870a6cad8bc3f7697faca732efb699e965af62
DiegoArrechea <diego.arrechea.unlp@gmail.com>
2023-08-16 22:41:02 -0300

    Cambio EJS

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require("express");
 
 const app = express();
 
+// Configurar el motor de plantillas EJS
+app.set('view engine', 'ejs');
+
+// Middleware para analizar los datos enviados en el cuerpo de la solicitud
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Ruta principal
 app.get("/", (req, res) => {
-    res.send("Express on Vercel");
+    // Renderizar la vista 'header.ejs'
+    res.render('header.ejs');
 });
 
+// Escuchar en el puerto 5000
 app.listen(5000, () => {
     console.log("Running on port 5000.");
 });
-
-// Export the Express API
-module.exports = app;
