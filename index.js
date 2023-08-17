a239605175e76917bbde2bd920890af2fedf7a4b
DiegoArrechea <diego.arrechea.unlp@gmail.com>
2023-08-16 23:00:34 -0300

    Agregandoi set views

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require('path');
 const app = express();
 
 // Configurar el motor de plantillas EJS
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 
 // Configuración para servir archivos estáticos (HTML, CSS, JS, imágenes, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.set("views", path.join(__dirname, "views"));
 // Middleware para analizar los datos enviados en el cuerpo de la solicitud
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,5 +20,5 @@ app.get("/", (req, res) => {
 
 // Escuchar en el puerto 5000
 app.listen(5000, () => {
-    console.log("Running on port 5000.");
+    console.log("Running on port http://localhost:5000/.");
 });
